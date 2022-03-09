3d16a3d1f47ac724bf7cbf6968a5a5274c57ee28
Andruw Sorensen <andruw.sorensen@gmail.com>
2022-03-09 10:41:54 -0700

    Completed Game

--- Light_Cycles/Light_Cycles_Game/Light_Cycles/game/scripting/handle_collisions_action.py
+++ Light_Cycles/Light_Cycles_Game/Light_Cycles/game/scripting/handle_collisions_action.py
@@ -57,18 +57,14 @@ class HandleCollisionsAction(Action):
         head = cycle.get_segments()[0]
         head2 = cycle2.get_segments()[0]
         segments = cycle.get_segments()[1:]
-        segments2 = cycle.get_segments()[1:]
+        segments2 = cycle2.get_segments()[1:]
         
         for segment in segments:
-            if head.get_position().equals(segment.get_position()):
-                self._is_game_over = True
-            elif head2.get_position().equals(segment.get_position()):
+            if head.get_position().equals(segment.get_position()) or head2.get_position().equals(segment.get_position()):
                 self._is_game_over = True
         
         for segment in segments2:
-            if head.get_position().equals(segment.get_position()):
-                self._is_game_over = True
-            elif head2.get_position().equals(segment.get_position()):
+            if head.get_position().equals(segment.get_position()) or head2.get_position().equals(segment.get_position()):
                 self._is_game_over = True
         
     def _handle_game_over(self, cast):
