441364cf697a5f4d02a38cd1585fe193203b0886
Andruw Sorensen <andruw.sorensen@gmail.com>
2022-03-09 08:44:20 -0700

    Got rid of food

--- cycle/cycle/cycle/__main__.py
+++ cycle/cycle/cycle/__main__.py
@@ -1,7 +1,6 @@
 import constants
 
 from game.casting.cast import Cast
-from game.casting.food import Food
 from game.casting.score import Score
 from game.casting.cycle import Cycle
 from game.scripting.script import Script
@@ -20,8 +19,7 @@ def main():
     
     # create the cast
     cast = Cast()
-    cast.add_actor("foods", Food())
-    cast.add_actor("cycles", Cycles())
+    cast.add_actor("cycles", Cycle())
     cast.add_actor("scores", Score())
    
     # start the game
